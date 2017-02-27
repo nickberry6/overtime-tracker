506799ce19255b9340c1a8f6037d2b1f6efe9391
Nick Berry <nicholasberry.13@gmail.com>
2017-02-26 21:15:22 -0500

    implemented user connection to post with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'posts' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Smith")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -25,10 +27,17 @@ describe 'posts' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some Rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some Rationale")
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User_Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
 end
