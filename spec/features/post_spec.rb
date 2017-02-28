5e8c84a1a7c0664ccb41751d4293f374eece84a8
Nick Berry <nicholasberry.13@gmail.com>
2017-02-28 04:51:29 -0500

    implemented edit and update functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -52,4 +52,26 @@ describe 'posts' do
       expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
+
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post, user_id: @user.id)
+    end
+
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
+
+      click_link("edit_#{@post.id}")
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "edited content"
+      click_on "Save"
+      expect(page.status_code).to eq(200)
+    end
+  end
 end
