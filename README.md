f7e7484b4a1551ba7edce15c3aa9110b27261956
Nick Berry <nicholasberry.13@gmail.com>
2017-03-05 18:13:11 -0500

    locking records to users that have been approved

--- README.md
+++ README.md
@@ -21,5 +21,4 @@
 - x Update the styles for forms
 
 ## Refactor TODOS:
-- Refactor user association integration test in post_spec
 - Refactor posts/form for admin user with status
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -28,5 +28,16 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
+
+    it 'should not be editable by the post creator if the status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      @post.user_id = user.id
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
